3635566736317cd2b85941bd50d229dede232d6e
Pujith Kachana <pkachana3@gatech.edu>
2020-10-18 01:20:27 -0400

    Second Commit

--- app/src/main/java/mentalhealth/app/ProfileFragment.kt
+++ app/src/main/java/mentalhealth/app/ProfileFragment.kt
@@ -19,9 +19,17 @@ class ProfileFragment : Fragment(R.layout.fragment_profile), View.OnClickListene
     lateinit var navController: NavController
     private val RC_SIGN_IN = 123
 
+    var totalMood = 0.0
+    var totalClicks = 0.0
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<Button>(R.id.signout_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.m1).setOnClickListener(this)
+        view.findViewById<Button>(R.id.m2).setOnClickListener(this)
+        view.findViewById<Button>(R.id.m3).setOnClickListener(this)
+        view.findViewById<Button>(R.id.m4).setOnClickListener(this)
+        view.findViewById<Button>(R.id.m5).setOnClickListener(this)
 
         profileName.text=Firebase.auth.currentUser!!.displayName
     }
@@ -38,6 +46,50 @@ class ProfileFragment : Fragment(R.layout.fragment_profile), View.OnClickListene
                 .setAvailableProviders(providers)
                 .build(),
                 RC_SIGN_IN)
+        } else {
+            var moodRating = 0.0
+
+            if (v!!.getId() == R.id.m1) {
+                totalMood += 1
+                totalClicks++
+                moodRating = 1.0
+            }
+            if (v!!.getId() == R.id.m2) {
+                totalMood += 2
+                totalClicks++
+                moodRating = 2.0
+            }
+            if (v!!.getId() == R.id.m3) {
+                totalMood += 3
+                totalClicks++
+                moodRating = 3.0
+            }
+            if (v!!.getId() == R.id.m4) {
+                totalMood += 4
+                totalClicks++
+                moodRating = 4.0
+            }
+            if (v!!.getId() == R.id.m5) {
+                totalMood += 5
+                totalClicks++
+                moodRating = 5.0
+            }
+            var avg = Math.round((totalMood/totalClicks) * 100.0) / 100.0
+            if (moodRating < 3) {
+                feedback.text = "\nThat's concerning! You should consider talking to me through the chat"
+            } else if(moodRating < 5) {
+                feedback.text = "\nThat's good! Keep improving!"
+            } else {
+                feedback.text = "\nThat's great! Keep it up!!"
+            }
+            if ((moodRating < avg) && (totalClicks > 1)) {
+                feedback2.text = "\nOh no, that lower than usual! Your average is $avg"
+            } else if ((moodRating == avg) && (totalClicks > 1)){
+                feedback2.text = "\nThat's around your average mood! Your average is $avg"
+            } else if ((moodRating >= avg) && (totalClicks > 1)){
+                feedback2.text = "\nThat's better than usual! Your average is $avg"
+            }
+
         }
     }
 }
