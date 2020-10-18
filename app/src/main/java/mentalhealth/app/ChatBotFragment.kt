31a8d0bafebe3120beba7f1c571c7074ea364217
edwardlee <supereduardus@gmail.com>
2020-10-18 00:21:52 -0500

    Chatbot

--- app/src/main/java/mentalhealth/app/ChatBotFragment.kt
+++ app/src/main/java/mentalhealth/app/ChatBotFragment.kt
@@ -1,13 +1,16 @@
 package mentalhealth.app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_chat_bot.*
+
 
 class ChatBotFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -16,4 +19,11 @@ class ChatBotFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_chat_bot, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        webView.loadUrl("https://console.dialogflow.com/api-client/demo/embedded/33b70d6a-2e7c-4517-bb6c-9b6d106b4a6f")
+        webView.webViewClient = WebViewClient()
+        val webSettings: WebSettings = webView.getSettings()
+        webSettings.javaScriptEnabled = true
+    }
 }
--- app/src/main/java/mentalhealth/app/WeeklyStatsFragment.kt
+++ app/src/main/java/mentalhealth/app/WeeklyStatsFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.fitness.Fitness
+import com.google.android.gms.fitness.data.DataType
+import com.google.android.gms.fitness.data.Field
+import kotlinx.android.synthetic.main.fragment_weekly_stats.*
 
 class WeeklyStatsFragment : Fragment() {
 
@@ -14,5 +19,20 @@ class WeeklyStatsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_weekly_stats, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        WeeklyStatsPH.text = "test"
+        Fitness.getHistoryClient(requireActivity(),
+            GoogleSignIn.getLastSignedInAccount(requireActivity())!!
+        )
+            .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
+            .addOnSuccessListener {
+                WeeklyStatsPH.text = "Steps: ${it.dataPoints[0].getValue(Field.FIELD_STEPS)}"
+            }
+            .addOnFailureListener {
+                WeeklyStatsPH.text = "Error"
+            }
     }
 }
