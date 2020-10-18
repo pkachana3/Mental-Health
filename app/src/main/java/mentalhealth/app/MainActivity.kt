741bfbd2aa168db51555757b888fb7b60d05d962
edwardlee <supereduardus@gmail.com>
2020-10-18 00:55:26 -0500

    fullscreen and exit

--- app/src/main/java/mentalhealth/app/MainActivity.kt
+++ app/src/main/java/mentalhealth/app/MainActivity.kt
@@ -3,8 +3,10 @@ package mentalhealth.app
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.firebase.ui.auth.AuthUI
@@ -27,6 +29,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val currentApiVersion = Build.VERSION.SDK_INT
+        val flags: Int = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        if (currentApiVersion >= Build.VERSION_CODES.KITKAT) {
+            window.decorView.systemUiVisibility = flags
+            val decorView: View = window.decorView
+            decorView.setOnSystemUiVisibilityChangeListener { visibility ->
+                if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN === 0) {
+                    decorView.systemUiVisibility = flags
+                }
+            }
+        }
 
         signIn()
 
