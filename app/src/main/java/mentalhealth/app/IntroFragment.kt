f4e98582ea9e12b8f4c48d9ed304c8364cdebea4
Pujith Kachana <pkachana3@gatech.edu>
2020-10-17 23:13:18 -0400

    Second Commit

--- app/src/main/java/mentalhealth/app/IntroFragment.kt
+++ app/src/main/java/mentalhealth/app/IntroFragment.kt
@@ -12,24 +12,22 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_home_page.*
 
-class LoginFragment : Fragment(R.layout.fragment_login), View.OnClickListener {
+class IntroFragment : Fragment(R.layout.fragment_intro), View.OnClickListener {
 
     lateinit var navController: NavController
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
-        view.findViewById<Button>(R.id.emergency_btn).setOnClickListener(this)
-        view.findViewById<Button>(R.id.introCont_btn).setOnClickListener(this)
-
+        view.findViewById<Button>(R.id.contHomePage_btn).setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when(v!!.id){
-            R.id.emergency_btn -> navController!!.navigate(R.id.action_loginFragment_to_emergencyFragment)
-            R.id.introCont_btn-> navController!!.navigate(R.id.action_loginFragment_to_introFragment)
+            R.id.contHomePage_btn -> navController!!.navigate(R.id.action_introFragment_to_homePageFragment)
         }
     }
 
-}
 
+
+}
--- app/src/main/java/mentalhealth/app/ProfileFragment.kt
+++ app/src/main/java/mentalhealth/app/ProfileFragment.kt
@@ -5,15 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_profile.*
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(R.layout.fragment_profile), View.OnClickListener {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    lateinit var navController: NavController
+    private val RC_SIGN_IN = 123
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.signout_btn).setOnClickListener(this)
+
+        profileName.text=Firebase.auth.currentUser!!.displayName
     }
 
+    override fun onClick(v: View?) {
+        if (v!!.getId() == R.id.signout_btn) {
+            FirebaseAuth.getInstance().signOut()
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.GoogleBuilder().build(),
+                AuthUI.IdpConfig.EmailBuilder().build()
+            )
+            startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+                RC_SIGN_IN)
+        }
+    }
 }
